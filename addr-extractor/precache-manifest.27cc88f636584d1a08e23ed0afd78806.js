2bad1ab2563b310be1f600872776b06fedc24ca7
Lei DONG <dongleivip@gmail.com>
2022-11-23 12:35:03 +0800

    Publish video address extractor

---
+++ addr-extractor/precache-manifest.27cc88f636584d1a08e23ed0afd78806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e81eb5be3a41e1ea90f3",
+    "url": "/static/css/main.6b4ebfbe.chunk.css"
+  },
+  {
+    "revision": "e81eb5be3a41e1ea90f3",
+    "url": "/static/js/main.e81eb5be.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "/static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "8cc168a02ff534a81d71",
+    "url": "/static/js/2.8cc168a0.chunk.js"
+  },
+  {
+    "revision": "f42220c51556092d1319",
+    "url": "/static/js/runtime~main.f42220c5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8a597e2f347e4e840a456d5061514bb1",
+    "url": "/index.html"
+  }
+];
